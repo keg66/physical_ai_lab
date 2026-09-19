93c81c8a201dd39d872d7830819fae23bb783599
keg66 <keg@keg66.com>
2026-09-19 18:08:36 +0900

    day1: add world_state.py

---
+++ day1/world_state.py
@@ -0,0 +1,44 @@
+from enum import Enum
+
+from pydantic import BaseModel, Field
+
+
+class ObjectType(str, Enum):
+    BOX = "box"
+    OBSTACLE = "obstacle"
+
+
+class ObjectColor(str, Enum):
+    RED = "red"
+    GREEN = "green"
+    BLUE = "blue"
+    BLACK = "black"
+
+
+class Position(BaseModel):
+    """A point in image coordinates."""
+
+    x: int = Field(ge=0)
+    y: int = Field(ge=0)
+
+
+class BoundingBox(BaseModel):
+    """A bounding box represented by its center and its dimensions."""
+
+    center: Position
+    width: int = Field(gt=0)
+    height: int = Field(gt=0)
+
+
+class ObjectProperties(BaseModel):
+    color: ObjectColor
+
+
+class WorldObject(BaseModel):
+    type: ObjectType
+    bbox: BoundingBox
+    properties: ObjectProperties
+
+
+class WorldState(BaseModel):
+    objects: list[WorldObject] = Field(default_factory=list)
